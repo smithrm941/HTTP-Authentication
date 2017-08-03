b0253e88547d8be585ee645aaf3a465d9c7844f6
smithrm941 <smithrm941@gmail.com>
2017-08-03 14:35:08 -0700

    installed dependencies, created necessary files

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieSession = require('cookie-session')
+const app = express();
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+})
