05884cb0531e52f7104bd0070ffb5101ddaa223b
smithrm941 <smithrm941@gmail.com>
2017-08-03 15:53:24 -0700

    added links and (non-working) sign up and login forms

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ const bodyParser = require('body-parser')
 const cookieSession = require('cookie-session')
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
 
+app.set('view engine', 'ejs')
 
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
+app.get('/signup', (req, res) => {
+  res.render('signup')
+})
 
+app.get('/login', (req, res) => {
+  res.render('login')
+})
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
